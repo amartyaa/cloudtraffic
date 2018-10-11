ed6e03960504f2ba6fc828596f5a89f4ed6bd2be
Amartya Anshuman <amartyanshuman.thakur@gmail.com>
2018-10-11 20:24:09 +0530

    Add files via upload
    Now supports 2 IR sensor...connected at PIN 14 and PIN 16.{GPIO.BCM}

---
+++ Mini_project_v3.py
@@ -0,0 +1,83 @@
+import os
+import bluetooth
+import RPi.GPIO as GPIO
+from google.cloud import firestore
+
+############Explicit Credential environment
+path="/home/pi/Desktop/Cloud-Parking-f348563ba8bf.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =path
+
+"""##########Initialize bluetooth
+server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+port = 1
+server_socket.bind(("",port))
+server_socket.listen(1)
+client_socket,address = server_socket.accept()
+print ("Accepted connection from ",address)
+####Bluetooth connection over """
+
+#GPIO starts
+s1=14
+s2=16
+GPIO.setmode(GPIO.BCM)     
+GPIO.setwarnings(False)
+GPIO.setup(s1,GPIO.IN)
+GPIO.setup(s2,GPIO.IN)
+
+
+
+#firestore initialization
+db = firestore.Client()
+doc_ref_s1 = db.collection(u'sensor').document(u'Sensor1')				#node sensor1 in #node sensor
+doc_ref_s2 = db.collection(u'sensor').document(u'Sensor1')
+#here starts main
+data1=0
+data2=0
+while 1:
+	
+	"""
+	d = client_socket.recv(1024)
+	c=str(d)
+	data=c[2]"""
+	if(GPIO.input(s1)==True): #no car found in slot 1
+		data1=0
+	else: data1=1
+        
+	print("Received: %s" % data1)
+	if(data1=="0"):
+		(
+		doc_ref_s1.update({
+		u'Car': u'Absent',				#in field  , value form
+		u'bin': 0
+		})
+		
+		)
+	elif(data1=="1"):
+		(
+		#print("Sent: %s" % data)
+		doc_ref_s1.update({
+		u'Car': u'Present',	#in field  , value form
+		u'bin': 1
+		})
+		)
+	###Now starts for sensor 2
+	if(GPIO.input(s2)==True): #no car found in slot 1
+		data1=0
+	else: data1=1
+        
+	print("Received: %s" % data2)
+	if(data2=="0"):
+		(doc_ref_s2.update({
+		u'Car': u'Absent',				#in field  , value form
+		u'bin': 0
+		})
+		
+		)
+	elif(data2=="1"):
+		(
+		doc_ref_s2.update({
+		u'Car': u'Present',	#in field  , value form
+		u'bin': 1
+		})
+		)
+
