7cde8a14d41e7cb9fe790d3481e4a756f4ab1414
Amartya Anshuman <amartyanshuman.thakur@gmail.com>
2018-10-17 11:20:42 +0530

    V5
    ChangeLog
    V5
    1) Cloud Server changed
    2) Updated parameters
    3) Location added on the cloud side

---
+++ Mini_project_v5.py
@@ -0,0 +1,60 @@
+import os
+import RPi.GPIO as GPIO
+from google.cloud import firestore
+
+############Explicit Credential environment
+path="/home/pi/Desktop/shreshthParking.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =path
+
+#GPIO starts
+s1=2
+s2=21
+GPIO.setmode(GPIO.BCM)     
+GPIO.setwarnings(False)
+GPIO.setup(s1,GPIO.IN)
+GPIO.setup(s2,GPIO.IN)
+
+
+#firestore initialization
+db = firestore.Client()
+doc_ref_s1 = db.collection(u'sensors').document(u'sensor1')			
+doc_ref_s2 = db.collection(u'sensors').document(u'sensor2')
+#here starts main
+data1=0
+data2=0
+while 1:
+	if(GPIO.input(s1)==False): #car found in slot 1
+		data1=1
+	else: data1=0
+        
+	print("Received from 1: %s" % data1)
+	if(str(data1)=="0"):
+		(
+		doc_ref_s1.update({
+		u'occupancy': 0
+		})
+		)
+	elif(str(data1)=="1"):
+		(
+		doc_ref_s1.update({
+		u'occupancy': 1
+		})
+		)
+	###Now starts for sensor 2
+	if(GPIO.input(s2)==False): #car found in slot 2
+		data2=1
+	else: data2=0
+        
+	print("Received from 2: %s" % data2)
+	if(str(data2)=="0"):
+		(
+		doc_ref_s2.update({
+		u'occupancy': 0
+		})
+		)
+	elif(str(data2)=="1"):
+		(
+		doc_ref_s2.update({
+		u'occupancy': 1
+		})
+		)
